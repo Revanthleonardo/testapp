252e9a516104d9824af5cc68af93d3ef063ff023
Revanthleonardo <revanthapollo@gmail.com>
2023-12-01 00:10:32 +0530

    dd

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-app = FastAPI()
+uri = "mongodb+srv://rev:4qXHNy2rBXNwmTeq@cluster.kigfmdr.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
     
 
-@app.get("/items/{item_id}")
-def read_item(item_id: str):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: str):
+#     return {"item_id": item_id}
+
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
