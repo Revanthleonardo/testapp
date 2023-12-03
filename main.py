fcf712fd265f35e1345389ca298215923c3cde3f
Revanth leonardo <revanthapollo@gmail.com>
2023-12-03 22:49:24 +0530

    final

--- main.py
+++ main.py
@@ -1,27 +1,71 @@
-from fastapi import FastAPI, Depends, HTTPException
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
 
 app = FastAPI()
 
-# MongoDB connection details
-mongo_uri = "mongodb://localhost:27017/"
+# MongoDB Connection
+mongo_client = AsyncIOMotorClient("mongodb://localhost:27017")
+database = mongo_client["testdb"]
+collection = database["items"]
 
-# Dependency to establish MongoDB connection
-def get_db():
-    try:
-        client = MongoClient(mongo_uri)
-        # Return the database connection to be used in route handlers
-        return client
-    except ConnectionFailure as e:
-        raise HTTPException(status_code=500, detail=f"Failed to connect to MongoDB: {e}")
 
-# Example route to check MongoDB connection
-@app.get("/check_mongodb_connection")
-def check_mongodb_connection(db: MongoClient = Depends(get_db)):
-    try:
-        # Check if the connection is successful by listing the databases
-        database_list = db.list_database_names()
-        return {"message": "Connected to MongoDB", "databases": database_list}
-    except ConnectionFailure as e:
-        raise HTTPException(status_code=500, detail=f"Failed to list databases in MongoDB: {e}")
+class Item(BaseModel):
+    name: str
+    description: str = None
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    # Convert Pydantic model to a dictionary
+    item_dict = item.dict()
+
+    # Insert document into MongoDB
+    result = await collection.insert_one(item_dict)
+    item_id = str(result.inserted_id)
+
+    # Return the created item with its new MongoDB ID
+    return {**item.dict(), "id": item_id}
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    # Find document in MongoDB by ID
+    result = await collection.find_one({"_id": ObjectId(item_id)})
+
+    # If document is not found, raise HTTP 404 Not Found
+    if result is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    # Convert MongoDB document to Pydantic model
+    return {**result, "id": item_id}
+
+
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    # Convert Pydantic model to a dictionary
+    item_dict = item.dict()
+
+    # Update document in MongoDB by ID
+    result = await collection.update_one({"_id": ObjectId(item_id)}, {"$set": item_dict})
+
+    # If document is not found, raise HTTP 404 Not Found
+    if result.modified_count == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    # Return the updated item
+    return {**item.dict(), "id": item_id}
+
+
+@app.delete("/items/{item_id}", response_model=dict)
+async def delete_item(item_id: str):
+    # Delete document in MongoDB by ID
+    result = await collection.delete_one({"_id": ObjectId(item_id)})
+
+    # If document is not found, raise HTTP 404 Not Found
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    # Return a response indicating the deletion
+    return {"message": "Item deleted successfully"}
